0abd02cc2f0cb759781f250a518b5cf5ebfb2e7e
Oswitto624 <dzirtsmd@mail.ru>
2022-02-28 01:52:57 +0300

    визуализация списка сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -8,8 +8,8 @@ namespace WebStore.Controllers
         private static readonly List<Employee> __Employees = new()
         {
             new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иваныч", Sex = "Male", Age = 23, WorkExperience = 3 },
-            new Employee { Id = 1, LastName = "Сидорова", FirstName = "Сидорыня", Patronymic = "Сидоровна", Sex = "Female", Age = 43, WorkExperience = 18 },
-            new Employee { Id = 1, LastName = "Петров", FirstName = "Петро", Patronymic = "Петрович", Sex = "Male", Age = 37, WorkExperience = 10 },
+            new Employee { Id = 2, LastName = "Сидорова", FirstName = "Сидорыня", Patronymic = "Сидоровна", Sex = "Female", Age = 43, WorkExperience = 18 },
+            new Employee { Id = 3, LastName = "Петров", FirstName = "Петро", Patronymic = "Петрович", Sex = "Male", Age = 37, WorkExperience = 10 },
         };
 
         private readonly IConfiguration _Configuration;
@@ -31,7 +31,7 @@ namespace WebStore.Controllers
             return Content($"config: {_Configuration["ServerGreetings"]}");
         }
 
-        public IActionResult Employees()
+        public IActionResult EmployeesList()
         {
             return View(__Employees);
         }

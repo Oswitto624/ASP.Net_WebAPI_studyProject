9f7dac3988833e36570519a79ef7f6ca2b4323b2
Oswitto624 <dzirtsmd@mail.ru>
2022-02-27 15:16:54 +0300

    Приветствие из контроллера

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,7 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 app.UseRouting();
-app.MapGet("/", () => app.Configuration["ServerGreetings"]);
-//app.MapDefaultControllerRoute();
+
+app.MapGet("/greetings", () => app.Configuration["ServerGreetings"]);
+
+app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();

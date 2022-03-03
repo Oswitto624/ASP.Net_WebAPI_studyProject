94dc010a6cb5dac72be715058a02a7b1781eafff
Oswitto624 <dzirtsmd@mail.ru>
2022-03-04 00:29:59 +0300

    Добавление и настройка статического содержимого

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -2,7 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+
 app.UseRouting();
+
+app.MapGet("/throw", () =>
+{
+    throw new ApplicationException("Пример ошибки приложения");
+});
 
 app.MapGet("/greetings", () => app.Configuration["ServerGreetings"]);
 

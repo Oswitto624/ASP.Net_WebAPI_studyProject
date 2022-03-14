bdb7d2e2a9336b1d15e231dc8a22ffabf6072b05
Oswitto624 <dzirtsmd@mail.ru>
2022-03-15 01:39:58 +0300

    Добавление контроллера и представления детальной информации о сотруднике

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -5,13 +5,6 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> __Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иваныч", Age = 23 },
-            new Employee { Id = 2, LastName = "Сидорова", FirstName = "Сидорыня", Patronymic = "Сидоровна", Age = 43 },
-            new Employee { Id = 3, LastName = "Петров", FirstName = "Петро", Patronymic = "Петрович", Age = 37 },
-        };
-
         private readonly IConfiguration _Configuration;
 
         public HomeController(IConfiguration Configuration) { _Configuration = Configuration; }
@@ -29,11 +22,6 @@ namespace WebStore.Controllers
         public IActionResult ConfigStr()
         {
             return Content($"config: {_Configuration["ServerGreetings"]}");
-        }
-
-        public IActionResult EmployeesList()
-        {
-            return View(__Employees);
         }
     }
 }

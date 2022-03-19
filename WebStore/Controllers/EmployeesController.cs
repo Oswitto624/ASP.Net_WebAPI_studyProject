60d81f210cb4e5c71f2e78b1f8475e0714cdba48
Максим Сухарев <sucharev_m@hotmail.com>
2022-03-19 23:45:24 +0300

    Работа контроллера через сервис

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,21 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebStore.Models;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Controllers;
 
 //[Route("Staff/{action=Index}/{id?}")]
 public class EmployeesController : Controller
 {
+    private readonly IEmployeesData _EmployeesData;
+    private readonly ILogger<EmployeesController> _Logger;
+
+    public EmployeesController(IEmployeesData EmployeesData, ILogger<EmployeesController> Logger)
+    {
+        _EmployeesData= EmployeesData;
+        _Logger = Logger;
+    }
 
     public IActionResult Index()
     {
-        return View(__Employees);
+        var employees = _EmployeesData.GetAll();
+        return View(employees);
     }
 
     //[Route("~/employees/info-({id:int})")]
     public IActionResult Details(int Id)
     {
-        var employee = __Employees.FirstOrDefault(e => e.Id == Id);
+        var employee = _EmployeesData.GetById(Id);
 
         if(employee == null)
             return NotFound();

7f7b7131202212c4f35d66ea12f73e4fa5862a3d
Oswitto624 <dzirtsmd@mail.ru>
2022-03-23 22:47:30 +0300

    товары

--- WebStore/Services/InMemoryProductData.cs
+++ WebStore/Services/InMemoryProductData.cs
@@ -1,4 +1,5 @@
 ﻿using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 
@@ -9,4 +10,20 @@ public class InMemoryProductData : IProductData
     public IEnumerable<Section> GetSections() => TestData.Sections;
 
     public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+    public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
+    {
+        IEnumerable<Product> query = TestData.Products;
+
+        //if (Filter != null && Filter.SectionId != null)
+        //    query = query.Where(p => p.SectionId == Filter.SectionId);
+
+        if(Filter?.SectionId is { } section_id)
+            query = query.Where(p => p.SectionId == Filter.SectionId);
+
+        if(Filter?.BrandId is { } brand_id)
+            query = query.Where(p => p.BrandId == Filter.BrandId);
+
+        return query;
+    }
 }

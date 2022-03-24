6416551d91e133bf3600707412d582da92bf96d1
Oswitto624 <dzirtsmd@mail.ru>
2022-03-24 23:14:48 +0300

    Подготовка сущностей для размещения в БД

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,8 +1,16 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities;
+
+//[Table("ProductBrands")]
+[Index(nameof(Name), IsUnique = true)]
 public class Brand : NamedEntity, IOrderedEntity
 {
+    //[Column("BrandOrder")]
     public int Order { get; set; }
+
+    public ICollection<Product> Products { get; set; } = new HashSet<Product>();
 }
--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -1,13 +1,26 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities;
 
+[Index(nameof(Name))]
 public class Product : NamedEntity, IOrderedEntity
 {
     public int Order { get; set; }
     public int SectionId { get; set; }
+
+    [ForeignKey(nameof(SectionId))]
+    [Required]
+    public Section Section { get; set; } = null!;
     public int? BrandId { get; set; }
+    
+    [ForeignKey(nameof(BrandId))]
+    public Brand? Brand { get; set; }
     public string ImageUrl { get; set; } = null!;
+    
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
 }

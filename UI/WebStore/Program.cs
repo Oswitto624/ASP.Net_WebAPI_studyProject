c258bb3f19fd32def46b7240b5d7aaee9fa4f7a2
Oswitto624 <dzirtsmd@mail.ru>
2022-04-22 00:05:19 +0300

    Удаление БД из основного проекта, рефакторинг приложения (удаление закомментированного и мусорного кода)

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,14 +1,8 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Identity;
-using WebStore.Infrastructure.Conventions;
-using WebStore.Infrastructure.Middleware;
 using WebStore.Interfaces.Services;
 using WebStore.Interfaces.TestAPI;
 using WebStore.Services.Services;
-using WebStore.Services.Services.InMemory;
-using WebStore.Services.Services.InSQL;
 using WebStore.WebAPI.Clients.Employees;
 using WebStore.WebAPI.Clients.Identity;
 using WebStore.WebAPI.Clients.Orders;
@@ -17,34 +11,12 @@ using WebStore.WebAPI.Clients.Values;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//регистрация сервисов
 var services = builder.Services;
-services.AddControllersWithViews( opt =>
-{
-    opt.Conventions.Add(new TestConvention());
-    opt.Conventions.Add(new AddAreasControllerRoute());
-});
+services.AddControllersWithViews();
 
 var configuration = builder.Configuration;
-//var db_connection_string_name = configuration["Database"];
-//var db_connection_string = configuration.GetConnectionString(db_connection_string_name);
 
-//switch (db_connection_string_name)
-//{
-//    case "SqlServer":
-//    case "DockerDB":
-//        services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(db_connection_string));
-//        break;
-
-//    case "Sqlite":
-//        services.AddDbContext<WebStoreDB>(opt => opt.UseSqlite(db_connection_string, o => o.MigrationsAssembly("WebStore.DAL.Sqlite")));
-//        break;
-//}
-
-//services.AddTransient<IDbInitializer, DbInitializer>();
-
-services.AddIdentity<User, Role>(/*opt => opt.*/)
-    //.AddEntityFrameworkStores<WebStoreDB>()
+services.AddIdentity<User, Role>()
     .AddDefaultTokenProviders();
 
 services.AddHttpClient("WebStoreAPIIdentity", client => client.BaseAddress = new(configuration["WebAPI"]))
@@ -91,16 +63,7 @@ services.ConfigureApplicationCookie(opt =>
     opt.SlidingExpiration = true;
 });
 
-//services.AddScoped<IEmployeesData, InMemoryEmployeesData>();
-//services.AddScoped<IProductData, InMemoryProductData>();
-//services.AddScoped<IProductData, SqlProductData>();
 services.AddScoped<ICartService, InCookiesCartService>();
-//services.AddScoped<IOrderService, SqlOrderService>();
-
-//services.AddHttpClient<IValuesService, ValuesClient>(client => client.BaseAddress = new(configuration["WebAPI"]));
-//services.AddHttpClient<IEmployeesData, EmployeesClient>(client => client.BaseAddress = new(configuration["WebAPI"]));
-//services.AddHttpClient<IProductData, ProductsClient>(client => client.BaseAddress = new(configuration["WebAPI"]));
-//services.AddHttpClient<IOrderService, OrdersClient>(client => client.BaseAddress = new(configuration["WebAPI"]));
 
 services.AddHttpClient("WebStoreAPI", client => client.BaseAddress = new(configuration["WebAPI"]))
     .AddTypedClient<IValuesService, ValuesClient>()
@@ -108,16 +71,9 @@ services.AddHttpClient("WebStoreAPI", client => client.BaseAddress = new(configu
     .AddTypedClient<IProductData, ProductsClient>()
     .AddTypedClient<IOrderService, OrdersClient>();
 
-//services.AddAutoMapper(Assembly.GetEntryAssembly());
 services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
-
-//using(var scope = app.Services.CreateScope())
-//{
-//    var db_initializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
-//    await db_initializer.InitializeAsync(RemoveBefore: false);
-//}
 
 if (app.Environment.IsDevelopment())
 {
@@ -128,38 +84,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-
 app.UseAuthentication();
 app.UseAuthorization();
 
-
-app.MapGet("/throw", () =>
-{
-    throw new ApplicationException("Пример ошибки приложения");
-});
-
-
-app.MapGet("/greetings", () => app.Configuration["ServerGreetings"]);
-
-//app.MapDefaultControllerRoute();
-
-app.UseMiddleware<TestMiddleware>();
-
-//app.MapControllerRoute(
-//    name: "ActionRoute",
-//    pattern: "{controller}.{action}({a}, {b})");
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapControllerRoute(
-        name: "ActionRoute",
-        pattern: "{controller}.{action}({a}, {b})"
-    );
-
     endpoints.MapControllerRoute(
         name: "areas",
         pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"

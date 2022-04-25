ffbfff13315720301df1d0e6253a6fb9451f3fab
Oswitto624 <dzirtsmd@mail.ru>
2022-04-25 23:29:36 +0300

    Провайдер системы длггирования для log4net

---
+++ Common/WebStore.Logging/Log4NetLoggerProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+using System.Xml;
+
+namespace WebStore.Logging;
+
+public class Log4NetLoggerProvider : ILoggerProvider
+{
+    private readonly string _СonfigurationFile;
+    private readonly ConcurrentDictionary<string, Log4NetLogger> _Loggers = new();
+
+    public Log4NetLoggerProvider(string ConfigurationFile) => _СonfigurationFile = ConfigurationFile;
+
+    public ILogger CreateLogger(string Category) =>
+        _Loggers.GetOrAdd(Category, static (category, configuration_file) =>
+        {
+            var xml = new XmlDocument();
+            xml.Load(configuration_file);
+            return new Log4NetLogger(category, xml["log4net"]!);
+        }, _СonfigurationFile);
+
+    public void Dispose()
+    {
+        var loggers = _Loggers.Values.ToArray();
+        _Loggers.Clear();
+        foreach (var logger in loggers)
+            log.Dispose();
+    }
+}

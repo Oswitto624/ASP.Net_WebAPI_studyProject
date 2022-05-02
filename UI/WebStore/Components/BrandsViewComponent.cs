8ed182177f275ac15f780e4b35c7eaa008858ff8
Oswitto624 <dzirtsmd@mail.ru>
2022-05-02 18:45:12 +0300

    Выделение активного бренда

--- UI/WebStore/Components/BrandsViewComponent.cs
+++ UI/WebStore/Components/BrandsViewComponent.cs
@@ -9,8 +9,11 @@ public class BrandsViewComponent : ViewComponent
     private readonly IProductData _ProductData;
     public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
 
-    public IViewComponentResult Invoke(string BrandId) => View(GetBrands());
-    
+    public IViewComponentResult Invoke(string BrandId)
+    {
+        ViewBag.BrandId = (int.TryParse(BrandId, out var id) ? id : (int?)null)!;
+        return View(GetBrands());
+    }
 
     private IEnumerable<BrandViewModel> GetBrands() =>
         _ProductData.GetBrands()

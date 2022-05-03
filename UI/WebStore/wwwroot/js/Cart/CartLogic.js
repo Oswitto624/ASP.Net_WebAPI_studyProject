fbc09e872624f60206338fb63124da1a454022de
Oswitto624 <dzirtsmd@mail.ru>
2022-05-03 15:56:18 +0300

    Завершение логики скрипта

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -3,19 +3,26 @@
         getCartViewLink: "",
         addToCartLink: "",
         decrementLink: "",
-        removeFromCartlink: ""
+        removeFromCartLink: ""
     },
 
     init: function(properties) {
         $.extend(Cart._properties, properties);
 
-        $(".add-to-cart").click(Cart.addToCart)
+        Cart.initEvents();
+    },
+
+    initEvents: function () {
+        $(".add-to-cart").click(Cart.addToCart);
+        $(".cart_quantity_up").click(Cart.incrementItem);
+        $(".cart_quantity_down").click(Cart.decrementItem);
+        $(".cart_quantity_delete").click(Cart.removeItem);
     },
 
     addToCart: function(e) {
         e.preventDefault();
 
-        var button = $(this);
+        const button = $(this);
         const id = button.data("id");
 
         $.get(Cart._properties.addToCartLink + "/" + id)
@@ -42,6 +49,102 @@
                 $("#cart-container").html(cartHtml);
             })
             .fail(function () { console.log("refreshCartView fail"); });
+    },
+
+    incrementItem: function (e) {
+        e.preventDefault();
+
+        const button = $(this);
+        const id = button.data("id");
+
+        const tr = button.closest("tr");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function (response) {
+                console.log(response.message);
+
+                const count = parseInt($(".cart_quantity_input", tr).val());
+                $(".cart_quantity_input", tr).val(count + 1);
+
+                //refreshPrice(tr); // - грабли - вызов метода без указания "Cart".
+                Cart.refreshPrice(tr);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("incrementItem fail"); });
+    },
+
+    refreshPrice: function(tr) {
+        const count = parseInt($(".cart_quantity_input", tr).val());
+        const price = parseFloat($(".cart_price", tr).data("price"));
+
+        const totalPrice = price * count;
+
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+
+        const cartTotalPrice = $(".cart_total_price", tr);
+        cartTotalPrice.html(value);
+        cartTotalPrice.data("price", totalPrice);
+
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function () {
+        var totalPrice = 0;
+
+        $(".cart_total_price").each(function () {
+            const price = parseFloat($(this).data("price"));
+            totalPrice += price;
+        });
+
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+        $("#total-order-price").html(value);
+    },
+
+    decrementItem: function (e) {
+        e.preventDefault();
+
+        const button = $(this);
+        const id = button.data("id");
+        const tr = button.closest("tr");
+
+        $.get(Cart._properties.decrementLink + "/" + id)
+            .done(function (response) {
+                console.log(response.message);
+
+                const count = parseInt($(".cart_quantity_input", tr).val());
+
+                if (count > 1) {
+                    $(".cart_quantity_input", tr).val(count - 1);
+                    Cart.refreshPrice(tr);
+                } else {
+                    tr.remove();
+                    Cart.refreshTotalPrice();
+                }
+
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("decrementItem fail"); });
+
+
+    },
+
+    removeItem: function (e) {
+        e.preventDefault();
+
+        const button = $(this);
+        const id = button.data("id");
+
+        $.get(Cart._properties.removeFromCartLink + "/" + id)
+            .done(function (response) {
+                console.log(response.message);
+
+                button.closest("tr").remove();
+                Cart.refreshTotalPrice();
+                Cart.refreshCartView();
+
+            })
+            .fail(function () { console.log("removeItem fail"); });
+
 
     }
 }

38a1ee0df7c80ed3b4bcbcc336e1cbba91b72f29
Oswitto624 <dzirtsmd@mail.ru>
2022-05-05 00:00:05 +0300

    Подготовка рабочего места для скрипта

--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -8,6 +8,8 @@ namespace WebStore.Controllers;
 
 public class CatalogController : Controller
 {
+    private const string __CatalogPageSize = "CatalogPageSize";
+
     private readonly IProductData _ProductData;
     private readonly IConfiguration _Configuration;
 
@@ -20,7 +22,7 @@ public class CatalogController : Controller
     public IActionResult Index(int? SectionId, int? BrandId, int PageNumber = 1, int? PageSize = null)
     {
         var page_size = PageSize
-            ?? (int.TryParse(_Configuration["CatalogPageSize"], out var value) ? value : null);
+            ?? (int.TryParse(_Configuration[__CatalogPageSize], out var value) ? value : null);
 
         var filter = new ProductFilter
         {
@@ -56,5 +58,19 @@ public class CatalogController : Controller
             return NotFound();
 
         return View(product.ToView());
+    }
+
+    public IActionResult GetProductsView(int? SectionId, int? BrandId, int PageNumber = 1, int? PageSize = null)
+    {
+        var products = _ProductData.GetProducts(new()
+        {
+            BrandId = BrandId,
+            SectionId = SectionId,
+            PageNumber = PageNumber,
+            PageSize = PageSize ?? _Configuration.GetValue(__CatalogPageSize, 6)
+        });
+        var product_views = products.Items.OrderBy(p => p.Order).ToView();
+
+        return PartialView("Partial/_Products", product_views);
     }
 }

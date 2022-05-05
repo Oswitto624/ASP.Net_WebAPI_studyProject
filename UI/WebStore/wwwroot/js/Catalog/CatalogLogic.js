806b8dea86e604545bf456dc460eb56fa0a28979
Oswitto624 <dzirtsmd@mail.ru>
2022-05-05 22:01:30 +0300

    Логика скрипта перелистывания страницы (асинхронный пейджинг)

---
+++ UI/WebStore/wwwroot/js/Catalog/CatalogLogic.js
@@ -0,0 +1,38 @@
+﻿Catalog = {
+    _properties: {
+        getViewLink: ""
+    },
+
+    init: properties => {
+        $.extend(Catalog._properties, properties);
+
+        $(".pagination li a").click(Catalog.clickOnPage);
+    },
+
+    clickOnPage: async function (e) {
+        e.preventDefault();
+        const button = $(this);
+
+        if (button.parent().hasClass("active")) return;
+
+        const container = $("#catalog-container");
+
+        const data = button.data();
+        let query = "";
+        for (let key in data)
+            if (data.hasOwnProperty(key))
+                query += `${key}=${data[key]}&`;
+
+        const response = await fetch(`${Catalog._properties.getViewLink}?${query}`);
+
+        if (response.ok) {
+            container.html(await response.text());
+
+            $(".pagination li").removeClass("active");
+            button.parent().addClass("active");
+
+        } else {
+            console.log("clickOnPage fail", response.status);
+        }
+    }
+}

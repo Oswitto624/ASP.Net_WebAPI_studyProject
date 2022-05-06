b20f264eaf928bb6119c77def2c12ee087add820
Oswitto624 <dzirtsmd@mail.ru>
2022-05-07 00:56:10 +0300

    Консольный клиент SignalR

--- UI/WebStore.ConsoleUI/Program.cs
+++ UI/WebStore.ConsoleUI/Program.cs
@@ -1,7 +1,28 @@
-﻿using WebStore.ConsoleUI;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using WebStore.ConsoleUI;
 
 WebAPITest.Start();
 
+var configuration = new ConfigurationBuilder()
+   .AddJsonFile("appsettings.json")
+   .Build();
 
+var builder = new HubConnectionBuilder();
+var connection = builder
+   .WithUrl(configuration["ChatAddress"])
+   .Build();
 
+using var registration = connection.On<string>("MessageFromServer", msg => Console.WriteLine("Сообщение от сервера {0}", msg));
+
+Console.Write("Ожидание запуска сервера. Нажмите Enter для продолжения.");
 Console.ReadLine();
+
+await connection.StartAsync();
+Console.WriteLine("Соединение установлено");
+
+while (true)
+{
+    var message = Console.ReadLine();
+    await connection.SendAsync("SendMessage", message);
+}
